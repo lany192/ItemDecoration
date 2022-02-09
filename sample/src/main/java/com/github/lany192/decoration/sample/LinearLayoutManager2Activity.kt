a39a878f46497b33dbb5251fb14649fb67e58f28
lany <lany192@qq.com>
2022-02-09 22:44:32 +0800

    update

--- sample/src/main/java/com/github/lany192/decoration/sample/LinearLayoutManager2Activity.kt
+++ sample/src/main/java/com/github/lany192/decoration/sample/LinearLayoutManager2Activity.kt
@@ -1,44 +1,37 @@
-package com.github.lany192.decoration.sample;
+package com.github.lany192.decoration.sample
 
-import android.graphics.Color;
-import android.os.Bundle;
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.github.lany192.decoration.LinearItemDecoration
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+class LinearLayoutManager2Activity : AppCompatActivity() {
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.viewholder.BaseViewHolder;
-import com.github.lany192.decoration.LinearItemDecoration;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class LinearLayoutManager2Activity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear_layout_manager2);
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        List<String> items = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            items.add("item" + i);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_manager2)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val items: MutableList<String> = ArrayList()
+        for (i in 0..99) {
+            items.add("item$i")
         }
-        LinearLayoutManager manager = new LinearLayoutManager(this);
-        manager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(manager);
-        recyclerView.addItemDecoration(new LinearItemDecoration(LinearLayoutManager.HORIZONTAL)
+        val manager = LinearLayoutManager(this)
+        manager.orientation = LinearLayoutManager.HORIZONTAL
+        recyclerView.layoutManager = manager
+        recyclerView.addItemDecoration(
+            LinearItemDecoration(LinearLayoutManager.HORIZONTAL)
                 .setColor(Color.RED)
-                .setWidth(4));
-        recyclerView.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_simple_horizontal, items) {
+                .setWidth(4)
+        )
+        recyclerView.adapter = object : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_simple_horizontal, items) {
 
-            @Override
-            protected void convert(BaseViewHolder helper, String item) {
-                helper.setText(R.id.textView, item);
+            override fun convert(holder: BaseViewHolder, item: String) {
+                holder.setText(R.id.textView, item)
             }
-        });
+        }
     }
 }
--- sample/src/main/java/com/github/lany192/decoration/sample/LinearLayoutManagerActivity.kt
+++ sample/src/main/java/com/github/lany192/decoration/sample/LinearLayoutManagerActivity.kt
@@ -1,80 +1,48 @@
-package com.github.lany192.decoration.sample;
+package com.github.lany192.decoration.sample
 
-import android.graphics.Color;
-import android.os.Bundle;
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.github.lany192.decoration.Divider
+import com.github.lany192.decoration.ItemDecoration
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+class LinearLayoutManagerActivity : AppCompatActivity() {
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.viewholder.BaseViewHolder;
-import com.github.lany192.decoration.Divider;
-import com.github.lany192.decoration.ItemDecoration;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class LinearLayoutManagerActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recyclerview);
-        recyclerView = findViewById(R.id.recyclerview);
-        List<String> items = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            items.add("item" + i);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recyclerview)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
+        val items: MutableList<String> = ArrayList()
+        for (i in 0..99) {
+            items.add("item$i")
         }
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.addItemDecoration(new ItemDecoration() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(object : ItemDecoration() {
 
-            @Override
-            public Divider getDivider(int itemPosition) {
-                switch (itemPosition) {
-                    case 0:
-                        return new Divider()
-                                .setBottom(Color.YELLOW, 2)
-                                ;
-                    case 1:
-                        return new Divider()
-                                .setBottom(Color.BLACK, 2)
-                                ;
-                    case 2:
-                        return new Divider()
-                                .setBottom(Color.RED, 4)
-                                ;
-                    case 3:
-                        return new Divider()
-                                .setBottom(Color.RED, 4, 16, 16)
-                                ;
-                    case 4:
-                        return new Divider()
-                                .setBottom(Color.RED, 2, 16, 32)
-                                ;
-                    case 5:
-                        return new Divider()
-                                .setBottom(Color.RED, 2)
-                                ;
-                    case 6:
-                        return new Divider()
-                                .setLeft(Color.RED, 3)
-                                .setRight(Color.RED, 8)
-                                .setBottom(Color.RED, 6)
-                                ;
-                    default:
-                        return super.getDivider(itemPosition);
+            override fun getDivider(position: Int): Divider? {
+                return when (position) {
+                    0 -> Divider().setBottom(Color.YELLOW, 2f)
+                    1 -> Divider().setBottom(Color.BLACK, 2f)
+                    2 -> Divider().setBottom(Color.RED, 4f)
+                    3 -> Divider().setBottom(Color.RED, 4f, 16f, 16f)
+                    4 -> Divider().setBottom(Color.RED, 2f, 16f, 32f)
+                    5 -> Divider().setBottom(Color.RED, 2f)
+                    6 -> Divider().setLeft(Color.RED, 3f)
+                        .setRight(Color.RED, 8f)
+                        .setBottom(Color.RED, 6f)
+                    else -> super.getDivider(position)
                 }
             }
-        });
-        recyclerView.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_simple, items) {
-
-            @Override
-            protected void convert(BaseViewHolder helper, String item) {
-                helper.setText(R.id.textView, item);
+        })
+        recyclerView.adapter =
+            object : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_simple, items) {
+                override fun convert(holder: BaseViewHolder, item: String) {
+                    holder.setText(R.id.textView, item)
+                }
             }
-        });
     }
 }
